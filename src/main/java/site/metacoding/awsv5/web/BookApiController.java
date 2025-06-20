188480eec03118aded0a9babc47f1d756d618163
HaEun <mjcho0130@gmail.com>
2025-06-20 09:55:43 +0900

    배포전깃헙테스트 수행

--- src/main/java/site/metacoding/awsv5/web/BookApiController.java
+++ src/main/java/site/metacoding/awsv5/web/BookApiController.java
@@ -1,18 +1,17 @@
 package site.metacoding.awsv5.web;
 
-import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
 import site.metacoding.awsv5.service.BookService;
 import site.metacoding.awsv5.web.dto.BookRespDto;
 import site.metacoding.awsv5.web.dto.BookSaveReqDto;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,11 +19,13 @@ public class BookApiController {
 
     private final BookService bookService;
 
+    //LB가 EC2에jar 배포후 / 로 헬스 체크하는 주소
     @GetMapping("/")
     public String home() {
         return "<h1>aws-v5</h1>";
     }
 
+    //책목록보는
     @PostMapping("/api/book")
     public ResponseEntity<?> bookSave(@RequestBody BookSaveReqDto reqDto) {
         BookRespDto respDto = bookService.책등록(reqDto);
